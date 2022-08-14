27b18703fc6604f776bbb011d02e6ced2c582870
mjms0214 <80668325+mjms0214@users.noreply.github.com>
2022-08-14 14:11:16 +0900

    Delete GongGuHaSong directory

--- GongGuHaSong/src/main/java/solabo/GongGuHaSong/repository/SellRepository.java
+++
@@ -1,9 +0,0 @@
-package solabo.GongGuHaSong.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import solabo.GongGuHaSong.domain.Sell;
-
-
-public interface SellRepository extends MongoRepository<Sell,String> {
-
-}
