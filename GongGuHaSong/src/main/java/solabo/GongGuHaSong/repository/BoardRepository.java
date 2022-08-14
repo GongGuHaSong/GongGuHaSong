ac923d828bc79e349e754e0106964ba5408175b0
mjkim <mjkim2@pabloair.com>
2022-08-14 14:17:16 +0900

    코드 재업로드

--- GongGuHaSong/src/main/java/solabo/GongGuHaSong/repository/BoardRepository.java
+++
@@ -1,7 +0,0 @@
-package solabo.GongGuHaSong.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import solabo.GongGuHaSong.domain.Board;
-
-public interface BoardRepository extends MongoRepository<Board,String> {
-}
--- GongGuHaSong/src/main/java/solabo/GongGuHaSong/repository/SellRepository.java
+++ GongGuHaSong/src/main/java/solabo/GongGuHaSong/repository/SellRepository.java
@@ -1,9 +1,12 @@
 package solabo.GongGuHaSong.repository;
 
+import org.springframework.data.domain.Example;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import solabo.GongGuHaSong.domain.Sell;
 
+import java.util.List;
+
 
 public interface SellRepository extends MongoRepository<Sell,String> {
-
+    List<Sell> findByTitle(String title);
 }
